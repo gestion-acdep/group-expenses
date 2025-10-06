245309bccd6689ec1285f60e8d754ce380194372
Pablo Andres Dealbera <dealberapablo07@gmail.com>
2025-10-06 19:13:55 -0300

    add graceful shutdown

--- server/src/index.ts
+++ server/src/index.ts
@@ -29,3 +29,20 @@ server.listen(port, () => {
     }
   })();
 });
+
+// Graceful shutdown
+process.on('SIGTERM', () => {
+  console.log('SIGTERM received, shutting down gracefully');
+  server.close(() => {
+    console.log('Server closed');
+    process.exit(0);
+  });
+});
+
+process.on('SIGINT', () => {
+  console.log('SIGINT received, shutting down gracefully');
+  server.close(() => {
+    console.log('Server closed');
+    process.exit(0);
+  });
+});
